50d35bcc6e74a8813d5147e348b3f03ffa3fba3c
Nathan Adams <nda@nda-drowzee.myfiosgateway.com>
2021-12-05 23:02:44 -0500

    Day 2 tasks.

---
+++ day2/PositionAimTracker.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+
+public class PositionAimTracker
+{
+    public static void main(String[] p_args)
+    {
+		String fileName = "input.txt";
+
+		try (Stream<String> stream = Files.lines(Paths.get(fileName)))
+        {
+            track(stream);
+
+		}
+        catch (IOException e)
+        {
+			e.printStackTrace();
+            return;
+		}
+    }
+
+    private static void track(Stream<String> p_stream)
+    {
+        String[] array = p_stream.toArray(String[]::new);
+        track(array);
+    }
+    private static void track(String[] p_array)
+    {
+        int x = 0;
+        int y = 0;
+        int aim = 0;
+        for (String s : p_array)
+        {
+            String[] parts = s.split(" ");
+            int magnitude = 0;
+            if (parts.length != 2)
+            {
+                continue;
+            }
+            String dir = parts[0];
+            magnitude = Integer.valueOf(parts[1]);
+            if ("forward".equals(dir))
+            {
+                x += magnitude;
+                y += magnitude*aim;
+            }
+            else if ("up".equals(dir))
+            {
+                aim -= magnitude;
+            }
+            else if ("down".equals(dir))
+            {
+                aim += magnitude;
+            }
+        }
+        System.out.println("X: " + x);
+        System.out.println("Y: " + y);
+        System.out.println("X*Y=" + (x*y));
+    }
+}
